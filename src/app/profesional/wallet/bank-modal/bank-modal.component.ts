b0ca67a302ab98b7c1f232c03fce5fbce7f9cba5
Armando Carvajal <armandoecr96@gmail.com>
2021-04-22 10:37:29 -0500

    View of form bank and first state of graph on wallet

---
+++ src/app/profesional/wallet/bank-modal/bank-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-modal',
+  templateUrl: './bank-modal.component.html',
+  styleUrls: ['./bank-modal.component.scss'],
+})
+export class BankModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/profesional/wallet/wallet.page.ts
+++ src/app/profesional/wallet/wallet.page.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MenuController, ModalController } from '@ionic/angular';
+import { Chart, LinearScale, BarController, CategoryScale, BarElement } from 'chart.js';
+
+import { BankModalComponent } from './bank-modal/bank-modal.component'
+
+Chart.register(
+  LinearScale,
+  BarController,
+  CategoryScale,
+  BarElement
+)
 
 @Component({
   selector: 'app-wallet',
@@ -8,26 +18,75 @@ import { MenuController } from '@ionic/angular';
 })
 export class WalletPage implements OnInit {
 
+  @ViewChild('barChart') barChart;
+
+  graphicWeek = false;
+
   constructor(
     private menuController: MenuController,
+    private modalController: ModalController,
   ) { }
 
   ngOnInit() {
+    
   }
 
-  ionViewWillEnter(){
+  ionViewDidEnter() {
+    this.barChartMethod();
+  }
+
+  barChartMethod() {   
+    this.barChart = new Chart(this.barChart.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE'],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: [
+            'rgba(50, 182, 221)',
+            'rgba(50, 182, 221)',
+            'rgba(50, 182, 221)',
+            'rgba(50, 182, 221)',
+            'rgba(50, 182, 221)',
+            'rgba(50, 182, 221)',
+            'rgba(50, 182, 221)',
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
+  ionViewWillEnter() {
     this.menuController.enable(true, 'profesional');
   }
 
-  openMenu(){
+  openMenu() {
     this.menuController.open();
   }
 
-  onClick(){
-    // do something awesome
+  openBankDataModal() {
+    this.modalController.create({
+      component: BankModalComponent,
+      cssClass: 'modalBanco',
+    }).then(modalEl => {
+      modalEl.present();
+    });
   }
 
-  imgProfile(){
+  backWalletMain() {
+    this.graphicWeek = false
+  }
+
+  imgProfile() {
     // do something awesome
   }
 
